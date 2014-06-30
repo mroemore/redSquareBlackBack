6929c8b415b190f039f92d6726f5f1c8c7d64b2f
Matthew Roe <matthew@webfactory.ie>
2014-06-30 17:19:35 +0100

    clicky clicky, draw square.

--- HelloCanvas.js
+++ HelloCanvas.js
@@ -30,13 +30,35 @@ function main() {
 	if(a_pos < 0){
 		console.log('failed to get storage location of a_pos'); 
 		return;
-	 }
-
-	 gl.vertexAttrib3f(a_pos, 0.0, 0.0, 0.0);
+	}
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+	canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_pos) }
 
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	gl.drawArrays(gl.POINTS, 0, 1); 
+}
+
+var g_points = [];
+
+function click(ev, gl, canvas, a_pos){
+	var x = ev.clientX;
+	var y = ev.clientY;
+	var rect = ev.target.getBoundingClientRect();
+
+	x = ((x - rect.left) - canvas.height/2)/(canvas.height/2);
+	y = (canvas.width/2 - (y - rect.top))/(canvas.width/2);
+
+	g_points.push(x);
+	g_points.push(y);
+
+	gl.clear(gl.COLOR_BUFFER_BIT);
+
+	var len = g_points.length;
+	for(var i = 0; i < len; i+=2) { 
+		gl.vertexAttrib3f(a_pos, g_points[i], g_points[i+1], 0.0);
+		gl.drawArrays(gl.POINTS, 0, 1);
+	}
 }
